fe6b3929c4a1cfce1e55a1ed9d5865d31887116a
ashleyyoakam <ashleyyoakam@ashleys-mbp.fios-router.home>
2016-09-27 01:27:47 -0400

    only 1 interest rate

--- src/TuitionCalculator.java
+++ src/TuitionCalculator.java
@@ -6,23 +6,19 @@ public class TuitionCalculator {
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Enter percent increase between 1st and 2nd year: ");
+		System.out.println("Enter percent increase each year: ");
 		double interest1 = input.nextDouble();
-		System.out.println("Enter percent increase between 2nd and 3rd year: ");
-		double interest2 = input.nextDouble();
-		System.out.println("Enter percent increase between 3rd and 4th year: ");
-		double interest3 = input.nextDouble();
+		
 		
 		System.out.println("Enter initial tution: ");
 		double tuition = input.nextDouble();
 		
 		double convertedrate1=(interest1/100)+1;
-		double convertedrate2=(interest2/100)+1;
-		double convertedrate3=(interest3/100)+1;
+	
 		
 		double year2amount = convertedrate1*tuition;
-		double year3amount = convertedrate2*year2amount;
-		double year4amount = convertedrate3*year3amount;
+		double year3amount = convertedrate1*year2amount;
+		double year4amount = convertedrate1*year3amount;
 
 		double totalTuition = tuition+year2amount+year3amount+year4amount;
 		
